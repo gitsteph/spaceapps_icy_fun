297659cdd3876cf05d5e4686630f22732aa7b3e8
Stephanie H. Nguyen <byronic.string@gmail.com>
2016-04-24 11:22:22 -0700

    Created data model and hooked it into server.

--- model.py
+++ model.py
@@ -1,21 +1,93 @@
 """Models and database functions"""
-
 from flask_sqlalchemy import SQLAlchemy
-
 from sqlalchemy.dialects.postgresql import JSON
-
-import psycopg2, os
-
+import os
 
 
 db = SQLAlchemy()
 
+#### TODO:  Complete __repr__ methods ####
 
 ####################################################################
 #Model definitions
 
-class Users(db.Model):
-	pass
+
+class User(db.Model):
+    """User of site.  Includes superusers."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, autoincrement=True, nullable=False, primary_key=True)
+    email = db.Column(db.String(40), nullable=False, unique=True)
+    name = db.Column(db.String(40), nullable=False)
+    password = db.Column(db.String(40), nullable=False)
+    superuser_status = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        """Helpful representation when printed."""
+        return "< User id=%s Name=%s >" % (self.id, self.name)
+
+
+class UserPath(db.Model):
+    """GPS paths for each trip a user takes."""
+
+    __tablename__ = "userpaths"
+
+    id = db.Column(db.Integer, autoincrement=True, nullable=False, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    gps_path = db.Column(db.JSON, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+
+class Photo(db.Model):
+    """Photos (ptrs to AWS) -- can be connected to specific userpaths."""
+
+    __tablename__ = "photos"
+
+    id = db.Column(db.Integer, autoincrement=True, nullable=False, primary_key=True)
+    photo_ptr = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    geocoordinates = db.Column(db.JSON)
+    public = db.Column(db.Integer, nullable=False, default=1)
+    userpath_id = db.Column(db.Integer, db.ForeignKey('userpaths.id'), nullable=True)
+
+
+class AudioVideoRecording(db.Model):
+    """Audio & video recording ptrs -- can be connected to userpaths."""
+
+    __tablename__ = "recordings"
+
+    id = db.Column(db.Integer, autoincrement=True, nullable=False, primary_key=True)
+    recording_ptr = db.Column(db.String, nullable=False)
+    start_dt = db.Column(db.DateTime, nullable=False)
+    end_dt = db.Column(db.DateTime, nullable=False)
+    public = db.Column(db.Integer, nullable=False, default=0)
+    userpath_id = db.Column(db.Integer, db.ForeignKey('userpaths.id'), nullable=True)
+
+
+class ReportType(db.Model):
+    """Types of events, like bears, holes, cracks, etc."""
+
+    __tablename__ = "reporttypes"
+
+    id = db.Column(db.Integer, autoincrement=True, nullable=False, primary_key=True)
+    rtype = db.Column(db.String, nullable=False)
+
+
+class Report(db.Model):
+    """Logs of actual reports made."""
+
+    __tablename__ = "reports"
+
+    id = db.Column(db.Integer, autoincrement=True, nullable=False, primary_key=True)
+    rtype_id = db.Column(db.Integer, db.ForeignKey('reporttypes.id'), nullable=False)
+    date_logged = db.Column(db.DateTime, nullable=False)
+    geocoordinates = db.Coordinates(db.JSON)
+    public = db.Column(db.Integer, nullable=False, default=1)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
 
 ##############################################################################
 # Helper functions for flask app
@@ -36,6 +108,5 @@ if __name__ == "__main__":
     # you in a state of being able to work with the database directly.
 
     from server import app
-    # connect_to_db(app)
+    connect_to_db(app)
     print "Connected to DB."
-
--- server.py
+++ server.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template, request, redirect, flash, session, jsonify
-
-# from model import connect_to_db, db
-
+from model import connect_to_db, db, User, UserPath, Photo, AudioVideoRecording, ReportType, Report
 import os
 
 
 app = Flask(__name__)
 
-# Required to use Flask sessions and the debug toolbar
 SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "ABCDEF")
 app.secret_key = SECRET_KEY
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
@@ -21,15 +18,12 @@ DEBUG = "NO_DEBUG" not in os.environ
 
 @app.route('/')
 def home():
-	# 
-	return render_template('home.html')
-
-
+    return render_template('home.html')
 
 
 ###############################################
 # Main
 
 if __name__ == '__main__':
-	# connect_to_db(app)
-	app.run(debug=DEBUG, host="0.0.0.0", port=PORT)
+    connect_to_db(app)
+    app.run(debug=DEBUG, host="0.0.0.0", port=PORT)
