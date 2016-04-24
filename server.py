44df43a6fbbfb70acc2686af6c488dd546086c4c
Corey Shott <jcshott@gmail.com>
2016-04-24 14:22:32 -0700

    post route to receive tracking info and putting in db added

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request, redirect, flash, session, jso
 from model import connect_to_db, db, User, UserPath, Photo, AudioVideoRecording, ReportType, Report
 
 import os
+import json
+import datetime
 
 
 app = Flask(__name__)
@@ -22,6 +24,40 @@ DEBUG = "NO_DEBUG" not in os.environ
 def home():
 	return render_template('home.html')
 
+@app.route('/track', methods=["POST"])
+def uploadInfo():
+	"""" will recieve a JSON with gps data from front-end and saves to DB"""
+	if request.method == "POST":
+		# get the JSON object with lat/long tracking data
+		info_json = request.get_json()
+		
+		path_info = info_json['path'] 
+		#list of coordinates - i.e. [{u'lat': 65.5, u'lng': 165.6}, {u'lat': 65.5, u'lng': 165.7}, {u'lat': 65.5, u'lng': 165.9}]
+		
+		# get user id from session
+		u_id = session.get("user_id", 1)
+
+		# get current datetime
+		uploaded_datetime = datetime.datetime.now()
+
+		new_tracking_entry = UserPath(user_id=u_id, gps_path=path_info, created_at=uploaded_datetime)
+		db.session.add(new_tracking_entry)
+		# list of reports made
+		reports_info = info_json['reports']
+		# go thru each report and grab the report made and coordinates
+		for report in reports_info:
+			# TODO: Change this so that instead of recieving text (i.e. "bear") of the type of report, we get the id to match what we have in db
+			if report['report'] == "bear":
+				rtype = 1
+			report_coordinates = {"lat": report['lat'], "lng": report['lng'] }
+
+		
+			new_report_entry =  Report(rtype_id=rtype, date_logged=uploaded_datetime, geocoordinates=report_coordinates, user_id=u_id)
+		
+			db.session.add(new_report_entry)
+		db.session.commit()
+
+		return "ok"
 
 ###############################################
 # Main
